c64982ad5db25436bd8103c827853d00c4f39fb7
liudong <liudong@localhost.localdomain>
2017-06-21 11:11:14 +0800

    add carousel

--- mysqldump/demo_person.sql
+++ mysqldump/demo_person.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.17, for Linux (x86_64)
 --
 -- Host: localhost    Database: demo
 -- ------------------------------------------------------
--- Server version	5.7.17-log
+-- Server version	5.7.18-0ubuntu0.17.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -48,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-06-04 20:25:56
+-- Dump completed on 2017-06-21 11:09:19
